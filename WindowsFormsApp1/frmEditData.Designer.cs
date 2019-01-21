a7cf8d980f051c5d0dbe228665c5810878a8d23d
HSRValle <henriquesrvalle@gmail.com>
2019-01-21 10:03:03 -0200

    Adicionado outro grupo de botões

--- WindowsFormsApp1/frmEditData.Designer.cs
+++ WindowsFormsApp1/frmEditData.Designer.cs
@@ -37,11 +37,17 @@
             this.dateTimePickerData = new System.Windows.Forms.DateTimePicker();
             this.dateTimePickerTime = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtJustificativa
             // 
-            this.txtJustificativa.Location = new System.Drawing.Point(250, 211);
+            this.txtJustificativa.Location = new System.Drawing.Point(249, 229);
             this.txtJustificativa.Multiline = true;
             this.txtJustificativa.Name = "txtJustificativa";
             this.txtJustificativa.Size = new System.Drawing.Size(331, 124);
@@ -50,7 +56,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(247, 195);
+            this.label1.Location = new System.Drawing.Point(246, 213);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 1;
@@ -69,23 +75,23 @@
             // 
             this.radUnico.AutoSize = true;
             this.radUnico.Checked = true;
-            this.radUnico.Location = new System.Drawing.Point(250, 157);
+            this.radUnico.Location = new System.Drawing.Point(0, 3);
             this.radUnico.Name = "radUnico";
-            this.radUnico.Size = new System.Drawing.Size(87, 17);
+            this.radUnico.Size = new System.Drawing.Size(88, 17);
             this.radUnico.TabIndex = 4;
             this.radUnico.TabStop = true;
-            this.radUnico.Text = "apenas para ";
+            this.radUnico.Text = "Apenas para ";
             this.radUnico.UseVisualStyleBackColor = true;
             this.radUnico.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radTodos
             // 
             this.radTodos.AutoSize = true;
-            this.radTodos.Location = new System.Drawing.Point(458, 157);
+            this.radTodos.Location = new System.Drawing.Point(207, 3);
             this.radTodos.Name = "radTodos";
-            this.radTodos.Size = new System.Drawing.Size(123, 17);
+            this.radTodos.Size = new System.Drawing.Size(124, 17);
             this.radTodos.TabIndex = 5;
-            this.radTodos.Text = "para todos os alunos";
+            this.radTodos.Text = "Para todos os alunos";
             this.radTodos.UseVisualStyleBackColor = true;
             // 
             // label2
@@ -101,21 +107,21 @@
             // 
             this.dateTimePickerData.Location = new System.Drawing.Point(249, 131);
             this.dateTimePickerData.Name = "dateTimePickerData";
-            this.dateTimePickerData.Size = new System.Drawing.Size(214, 20);
+            this.dateTimePickerData.Size = new System.Drawing.Size(207, 20);
             this.dateTimePickerData.TabIndex = 7;
             // 
             // dateTimePickerTime
             // 
             this.dateTimePickerTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePickerTime.Location = new System.Drawing.Point(482, 131);
+            this.dateTimePickerTime.Location = new System.Drawing.Point(462, 131);
             this.dateTimePickerTime.Name = "dateTimePickerTime";
             this.dateTimePickerTime.ShowUpDown = true;
-            this.dateTimePickerTime.Size = new System.Drawing.Size(98, 20);
+            this.dateTimePickerTime.Size = new System.Drawing.Size(118, 20);
             this.dateTimePickerTime.TabIndex = 8;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(250, 358);
+            this.button1.Location = new System.Drawing.Point(249, 376);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(331, 23);
             this.button1.TabIndex = 9;
@@ -123,22 +129,66 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(0, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(105, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Atribuir Presença";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(207, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(116, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Remover Presença";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radUnico);
+            this.panel1.Controls.Add(this.radTodos);
+            this.panel1.Location = new System.Drawing.Point(250, 157);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(331, 23);
+            this.panel1.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Location = new System.Drawing.Point(250, 182);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(331, 23);
+            this.panel2.TabIndex = 13;
+            // 
             // frmEditData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePickerTime);
             this.Controls.Add(this.dateTimePickerData);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.radTodos);
-            this.Controls.Add(this.radUnico);
             this.Controls.Add(this.lblData);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtJustificativa);
             this.Name = "frmEditData";
             this.Text = "frmEditData";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,5 +205,9 @@
         private System.Windows.Forms.DateTimePicker dateTimePickerData;
         private System.Windows.Forms.DateTimePicker dateTimePickerTime;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }

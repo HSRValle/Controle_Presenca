bad80a7f1d5a6297eb7380256c9ae1a9408ba9fe
HSRValle <henriquesrvalle@gmail.com>
2019-02-15 10:14:35 -0200

    Conectando ao ip fixo da máquina

---
+++ WindowsFormsApp1/Sql.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    static class Sql
+    {
+        public static String Conection(){
+            return "server=192.168.15.34;uid=omni;pwd=omnipassword;database=new_schema";
+        }
+    }
+}

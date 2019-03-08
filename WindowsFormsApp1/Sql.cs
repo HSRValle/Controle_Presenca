5590cce94ff58eb666c2ac1123e534ebaa8a5c47
HSRValle <henriquesrvalle@gmail.com>
2019-03-08 12:02:49 -0300

    Verificando banco de dados antes da conexão. Conexão alternativa no Wi-fi

--- WindowsFormsApp1/Sql.cs
+++ WindowsFormsApp1/Sql.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace WindowsFormsApp1
 {
     static class Sql
     {
-        public static String Conection(){
-            return "server=192.168.15.34;uid=omni;pwd=omnipassword;database=new_schema";
+        public static bool classDecision(string IP)
+        {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c ping "+IP);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            // *** Read the streams ***
+            // Warning: This approach can lead to deadlocks, see Edit #2
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exitCode = process.ExitCode;
+
+            Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+            Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+            Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+            Console.ReadLine();
+            process.Close();
+
+            if (output.Contains("TTL="))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static String Conection()
+        {
+            if (Sql.classDecision("192.168.15.34") == true)
+            {
+                return "server=192.168.15.34;uid=omni;pwd=omnipassword;database=new_schema";
+            }
+            else
+            {
+                return "server=192.168.15.33;uid=omni;pwd=omnipassword;database=new_schema";
+            }
+
+            
         }
     }
 }
